cf4bccad4dbed3e027839d62e7b6bd3037d38042
Tony Bowden <tony@tmtm.com>
2016-11-21 11:43:23 +0000

    Update URL
    Wikipedia page has been moved

--- scraper.rb
+++ scraper.rb
@@ -3,14 +3,14 @@
 
 require 'wikidata/fetcher'
 
-constituency_members = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_National_Assembly_of_Pakistan_(2013–)',
+constituency_members = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_14th_National_Assembly_of_Pakistan',
   before: '//span[@id="Reserved_seats_for_women"]',
   xpath: '//table[.//th[.="Constituency"]]//td[4]//a[not(@class="new")][1]/@title',
 )
 
-non_constituency_members = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_National_Assembly_of_Pakistan_(2013–)',
+non_constituency_members = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_14th_National_Assembly_of_Pakistan',
   after: '//span[@id="Reserved_seats_for_women"]',
   xpath: '//table[.//th[.="Name"]]//td[3]//a[not(@class="new")][1]/@title',
 )

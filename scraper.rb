4061526ceb9e4b1634096d97b5d22c90ba082d82
Tony Bowden <tony@tmtm.com>
2016-11-21 11:49:24 +0000

    Rewrite for new layout
    The two original tables have been consolidated, and we now have two
    different tables — one for all the original members, and one for the
    changes that have happened post-election.

--- scraper.rb
+++ scraper.rb
@@ -3,17 +3,17 @@
 
 require 'wikidata/fetcher'
 
-constituency_members = EveryPolitician::Wikidata.wikipedia_xpath(
+original_members = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_14th_National_Assembly_of_Pakistan',
-  before: '//span[@id="Reserved_seats_for_women"]',
+  before: '//span[@id="Membership_changes"]',
   xpath: '//table[.//th[.="Constituency"]]//td[4]//a[not(@class="new")][1]/@title',
 )
 
-non_constituency_members = EveryPolitician::Wikidata.wikipedia_xpath(
+changes = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_14th_National_Assembly_of_Pakistan',
-  after: '//span[@id="Reserved_seats_for_women"]',
-  xpath: '//table[.//th[.="Name"]]//td[3]//a[not(@class="new")][1]/@title',
+  after: '//span[@id="Membership_changes"]',
+  xpath: '//table[.//th[.="Member"]]//td[4]//a[not(@class="new")][1]/@title',
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: constituency_members | non_constituency_members })
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: original_members | changes })
 
